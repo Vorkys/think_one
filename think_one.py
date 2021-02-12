d403146f1fec5501ee274ea9f25a69db34c68339
Erik <75934309+Vorkys@users.noreply.github.com>
2021-02-12 22:24:09 +0100

    Update think_one.py

--- think_one.py
+++ think_one.py
@@ -1,1 +1,13 @@
+#for x in range(9):
+#    exec("string" + str(x) + " = 'Hello' + str(x)")
 
+#print(string6)
+
+class Animals:
+    def __init__(self, name, color, leg_num, tail, size, spec):
+        self.name = name
+        self.color = color
+        self.leg_num = leg_num
+        self.tail = tail
+        self.size = size
+        self.spec = spec
