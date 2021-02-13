a2fdcdb1e175763efe0256a8ee80e18bb61a0e62
Erik <75934309+Vorkys@users.noreply.github.com>
2021-02-13 02:45:31 +0100

    Update think_one3.0.py

--- think_one3.0.py
+++ think_one3.0.py
@@ -1,9 +1,5 @@
-#for x in range(9):
-#    exec("string" + str(x) + " = 'Hello' + str(x)")
 
-#print(string6)
-
-class Animals:
+class Animal():
     def __init__(self, name, color, leg_num, tail, size, spec):
         self.name = name
         self.color = color
@@ -11,3 +7,37 @@ class Animals:
         self.tail = tail
         self.size = size
         self.spec = spec
+
+    def whatis(self):
+        print("Zvire je {}, barv: {}, koncetin ma {}, ocas: {}, velikost {}, vlastnosti {}".format(self.name, self.color, self.leg_num, self.tail, self.size, self.spec))
+        #animal1.whatis()
+
+### creates new dynamic vyriables
+#_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#for x in range(9):
+#    exec("animal" + str(x) + " = _list[7], _list[5]")
+
+#print(animal4)
+######################################
+
+i = 1
+
+with open("animals.txt", "r", encoding = "utf-8") as _file:
+#while 1:
+#    _line = _file.readline()
+
+#    x = _line.split(", ")
+#    print(x)
+
+#    if not _line:
+#        break
+
+    for line in _file:
+        line = line.strip()
+        radek = line.split(", ")
+        exec("animal" + str(i) + " = Animal(radek[0], radek[1], radek[2], radek[3], radek[4], radek[5])")
+        i += 1
+        print(radek)
+
+animal3.whatis()
